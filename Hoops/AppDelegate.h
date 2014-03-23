df0e15e69891738a618fe202dd1f25af20c140ae
Alejandro Cruz Paz <vidriloco@gmail.com>
2014-03-22 21:25:56 -0600

    Initial Commit

---
+++ Hoops/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Hoops
+//
+//  Created by Alejandro Cruz Paz on 3/22/14.
+//  Copyright (c) 2014 Hoops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Hoops/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hoops
+//
+//  Created by Alejandro Cruz Paz on 3/22/14.
+//  Copyright (c) 2014 Hoops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
